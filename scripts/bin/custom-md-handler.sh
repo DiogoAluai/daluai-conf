1de83e25a51159c7ba27501e95b7fcb2517d7108
Diogo Aluai <diogoaluai@gmail.com>
2026-09-13 13:50:12 +0100

    Fix md handler for snap installed firefox

--- scripts/bin/custom-md-handler.sh
+++ scripts/bin/custom-md-handler.sh
@@ -1,15 +1,22 @@
 #!/usr/bin/env bash
 
+##
+## Handler for md files, meant for xdg-open
+##
+
 if [ $# != 1 ]; then
     echo "Usage: command <file.md>."
     exit 1
 fi
 
 mdfile=$1
-tmpHtmlFile="$(mktemp)"
-impTmpHtmlFile="$(mktemp)"
 
-md_to_html $mdfile > $tmpHtmlFile \
-  && improve_html $tmpHtmlFile > $impTmpHtmlFile \
-  && firefox $impTmpHtmlFile 2> /dev/null \
-  && rm $tmpHtmlFile
+htmlFile=$(mktemp)
+improvedHtmlFile=$(mktemp --suffix=.html)
+
+md_to_html $mdfile > $htmlFile
+improve_html $htmlFile > $improvedHtmlFile
+chmod +r $improvedHtmlFile
+cp $improvedHtmlFile .
+firefox $(basename $improvedHtmlFile) 2> /dev/null
+rm $(basename $improvedHtmlFile)
